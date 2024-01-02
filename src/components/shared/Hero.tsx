2f7a3580eeb16f7b9b74bed5a8dd7a8945f0b0dd
sanjeevnode <me.sanjeevks@gmail.com>
2024-01-02 23:54:09 +0530

    Hero animation

--- src/components/shared/Hero.tsx
+++ src/components/shared/Hero.tsx
@@ -22,14 +22,14 @@ const Hero = () => {
         className="absolute inset-0 w-full h-full z-0"
         initial={{ scale: 1.1 }}
         animate={{ scale: 1 }}
-        transition={{ duration: 3.5, ease: "easeOut" }}
+        transition={{ duration: 3.5, ease: "easeIn" }}
       />
       <div className="max-w-6xl  flex   min-[1200px]:justify-end px-6 md:px-10 z-10 min-[1200px]:px-0 justify-start items-center h-full  w-full">
         <div className="flex flex-col gap-6 w-full md:w-1/2">
           <motion.div
             initial={{ y: 50, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.3, delay: 0.5, ease: "linear" }}
+            transition={{ duration: 0.8, delay: 0.5, ease: "linear" }}
           >
             <span className="md:text-[20px] text-[18px]  text-white">
               Meet Consulting
@@ -46,59 +46,67 @@ const Hero = () => {
           </motion.div>
 
           <motion.div
-            onMouseEnter={parentHover}
-            onMouseLeave={parentUnhover}
-            animate={controls}
-            className="  relative  w-fit flex justify-center items-center cursor-pointer"
+            initial={{ y: 50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.8, delay: 0.7, ease: "linear" }}
           >
             <motion.div
-              initial={"unhover"}
+              onMouseEnter={parentHover}
+              onMouseLeave={parentUnhover}
               animate={controls}
-              variants={{
-                hover: {
-                  scale: 0,
-                  opacity: 0,
-                },
-                unhover: {
-                  scale: 1,
-                  opacity: 1,
-                },
-              }}
-              transition={{ duration: 0.3, ease: "easeInOut" }}
-              className="w-14 h-14 md:w-8 md:h-8  border rounded-[50%] "
-            ></motion.div>
-            <motion.div
-              initial={"unhover"}
-              animate={controls}
-              variants={{
-                hover: {
-                  scale: 1,
-                  opacity: 1,
-                },
-                unhover: {
-                  scale: 0,
-                  opacity: 0,
-                },
-              }}
-              transition={{ duration: 0.3, ease: "easeInOut" }}
-              className=" w-14 h-14 md:w-8 md:h-8 bg-white flex justify-center items-center rounded-[50%]  absolute inset-0 "
+              className="  relative  w-fit flex justify-center items-center cursor-pointer"
             >
-              <ArrowRight className="w-10 h-8 text-black" />
-            </motion.div>
-            <motion.div
-              initial={"unhover"}
-              animate={controls}
-              variants={{
-                hover: {
-                  x: 20,
-                },
-                unhover: {
-                  x: -30,
-                },
-              }}
-              transition={{ duration: 0.3, ease: "easeInOut" }}
-            >
-              <span className="text-xl text-white font-bold">Our Services</span>
+              <motion.div
+                initial={"unhover"}
+                animate={controls}
+                variants={{
+                  hover: {
+                    scale: 0,
+                    opacity: 0,
+                  },
+                  unhover: {
+                    scale: 1,
+                    opacity: 1,
+                  },
+                }}
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+                className="md:w-14 md:h-14 w-10 h-10  border rounded-[50%] "
+              ></motion.div>
+              <motion.div
+                initial={"unhover"}
+                animate={controls}
+                variants={{
+                  hover: {
+                    scale: 1,
+                    opacity: 1,
+                  },
+                  unhover: {
+                    scale: 0,
+                    opacity: 0,
+                  },
+                }}
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+                className=" md:w-14 md:h-14 w-10 h-10 bg-white flex justify-center items-center rounded-[50%]  absolute inset-0 "
+              >
+                <ArrowRight className="w-10 h-8 text-black" />
+              </motion.div>
+              <motion.div
+                initial={"unhover"}
+                animate={controls}
+                variants={{
+                  hover: {
+                    x: 20,
+                  },
+                  unhover: {
+                    x: -30,
+                  },
+                }}
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+              >
+                <span className="text-xl text-white font-bold">
+                  Our Services
+                </span>
+              </motion.div>
             </motion.div>
           </motion.div>
         </div>
