3c53f32cb9c0b49165ea57ada6a4c9431908bee2
sanjeevnode <me.sanjeevks@gmail.com>
2024-01-06 18:39:41 +0530

    minor changes

--- src/components/shared/NavBar/MobileNavItems.tsx
+++ src/components/shared/NavBar/MobileNavItems.tsx
@@ -20,24 +20,24 @@ const MobileNavItems = () => {
         <li key={item.name} className="w-full border-b flex flex-col ">
           <div className="w-full flex justify-between items-center">
             <SheetClose asChild>
-              <Link to={item.link} className="">
+              <Link to={item.link} className="cursor-pointer">
                 <span className="text-xl  text-black">{item.name}</span>
               </Link>
             </SheetClose>
             {item.subLinks && (
               <ChevronDown
-                className="w-5 h-5 ml-1 font-bold"
+                className="w-5 h-5 ml-1 font-bold cursor-pointer"
                 onClick={() => toggle(index)}
               />
             )}
           </div>
           {item.subLinks && (
             <Collapse isOpened={open == index}>
-              <ul className="flex-col gap-4  bg-white mt-4 flex">
+              <ul className="flex-col  bg-white mt-4 flex">
                 {item.subLinks.map((subItem) => (
                   <SheetClose asChild key={subItem.name}>
                     <Link to={subItem.link}>
-                      <li className="cursor-pointer text-black hover:outline px-6">
+                      <li className="cursor-pointer text-black md:hover:bg-primary/90 hover:text-white px-6  transition-all duration-200 ease-in-out py-1 ">
                         {subItem.name}
                       </li>
                     </Link>
